dc78ae1aa4124aed69f85cecd156532079ade481
Richard <richardjalba@gmail.com>
2021-06-29 18:00:33 -0700

    Commit

---
+++ client/build/precache-manifest.ee217723c9661c1d3a63b7706c933953.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7907631e4e92cf14ab3367d86d5ead0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a926246f8918566eeba3",
+    "url": "/static/css/main.c23cb77e.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a926246f8918566eeba3",
+    "url": "/static/js/main.1dd3cb93.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  },
+  {
+    "revision": "c89ef9d889f0d4d6b4de2ac48afc146e",
+    "url": "/static/media/the-latin-everything-background.c89ef9d8.PNG"
+  }
+]);
